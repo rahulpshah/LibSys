ec324a9afe8b8ef18c4dc42b75dceda938f60859
Ronak Ghadiya <ronakghadiya6892@gmail.com>
2015-09-26 02:22:03 -0400

    Validating Admin model for unit test

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ActiveRecord::Base
 	validates :email, :email_format => { :message => "not in correct format" }
 	validates_uniqueness_of :email
 	validates :password, length: 6..20
-	validates :name, presence: true, length: { maximum: 50 }
+	validates :name, presence: true
 	before_destroy :destroyable?
   	def destroyable?
     	name != "admin"
